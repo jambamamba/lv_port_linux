eaa4bafa06bec80025d20487a6a4007ea569befe
caliboy <cali@fornia>
2025-12-10 22:40:30 +0100

    wip

--- build.sh
+++ build.sh
@@ -8,17 +8,18 @@ set -xe
 source share/pins.txt
 source share/scripts/helper-functions.sh
 
-function buildpy() {
-    pip3 install setuptools
-    CC=clang \
-    CXX=clang++ \
-    LD_LIBRARY_PATH=/usr/local/lib \
-    python3 setup.py build_ext --verbose\
-    --build-lib='py-build' \
-    --build-temp='/tmp/py-build' \
-    -j $(nproc)
-    # python setup.py bdist_wheel
-}
+# Now done in cmake:
+# function buildpy() {
+#     pip3 install setuptools
+#     CC=clang \
+#     CXX=clang++ \
+#     LD_LIBRARY_PATH=/usr/local/lib \
+#     python3 setup.py build_ext --verbose\
+#     --build-lib='py-build' \
+#     --build-temp='/tmp/py-build' \
+#     -j $(nproc)
+#     # python setup.py bdist_wheel
+# }
 
 function buildelf() {
     local deps=(zlib debug_logger json_utils curl libssh2 utils)
@@ -37,39 +38,32 @@ function buildelf() {
 }
 
 function run() {
-    local py_script="$(pwd)/src/examples/hello-world/hello-world.py"
-    local config_json="$(pwd)/src/examples/hello-world/hello-world.json"
+    local input_file="$(pwd)/src/examples/hello-world/hello-world.py"
+    # local input_file="$(pwd)/src/examples/hello-world/hello-world.json"
+    local method="py"
 
     parseArgs $@
-    local input_file
-    if [ -f ${py_script} ]; then
-        input_file=${py_script}
-    elif [ -f ${config_json} ]; then
-        input_file=${config_json}
-    else
-        echo "missing input file"
-        exit -1
-    fi
     echo fs.inotify.max_user_watches=1048575 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
     echo "set confirm off" |sudo tee ~/.gdbinit
 
-    # ways of running:
-    pushd x86-build/bin
-    gdb -ex "run" --args ./lvglsim ${input_file}
-    popd
-
-    # pushd x86-build/bin
-    # ./lvglsim ../../src/configs/imageview.json
-    # ./lvglsim ../../src/py/main.py #must be in x86-build/bin folder to run like this
-    # popd
-
-    # pushd src/examples/hello-world
-    #  LD_LIBRARY_PATH=/usr/local/lib ::
-    # buildpy
-    # run
+    case ${method} in
+        elf)
+            pushd x86-build/bin
+            # gdb -ex "run" --args ./lvglsim ${input_file}
+            ./lvglsim ${input_file}
+            popd
+            ;;
+        py)
+            pushd ./src/examples/hello-world/
+            LD_LIBRARY_PATH=/usr/local/lib PYTHONPATH=../../../x86-build/ python hello-world.py
+            popd
+            ;;
+        *);;
+    esac
 }
 
 function main() {
     buildelf
+    run
 }
 main $@ |tee x86-build/build.log
