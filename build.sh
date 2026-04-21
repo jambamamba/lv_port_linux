c9b14d5abd54fababf3b177b92b3b717d1f4135c
cali <cali@fornia>
2026-04-21 20:37:48 +0200

    wip

--- build.sh
+++ build.sh
@@ -52,13 +52,25 @@ cmake -G Ninja\
 
 function startRdpServer() {
 
-    local pid=$(ps -xa|grep westo[n] || true);
-    if [ -z "$pid" ]; then
+    #local pid=$(ps -xa|grep westo[n] || true);
+    #if [ -z "$pid" ]; then
+	export WAYLAND_DISPLAY=$(ls $XDG_RUNTIME_DIR/wayland-?)
+	if [ ! -z $WAYLAND_DISPLAY ]; then
+		sudo rm -f $WAYLAND_DISPLAY
+	fi
+	sudo killall weston || true
         sudo -E weston --backend=rdp-backend.so --rdp-tls-cert=/usr/local/weston.keys/server.crt --rdp-tls-key=/usr/local/weston.keys/server.key --address=0.0.0.0 --port=3389 &
         echo "Starting RDP server..."
-    else
-        echo "RDP server is already running with  pid $pid"
-    fi
+        sleep 5	
+	#configure to allow remote viewing:
+	export WAYLAND_DISPLAY=$(ls $XDG_RUNTIME_DIR/wayland-?)
+	sudo chown $(id -u):$(id -g) $WAYLAND_DISPLAY
+	#run on remote host:
+	#sudo apt install remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret -y
+
+    #else
+    #    echo "RDP server is already running with  pid $pid"
+    #fi
 }
 
 function run() {
@@ -66,12 +78,9 @@ function run() {
     local example="imageview" # hello-world | hello-world-international |  | messagebox | stackview | tabview | testview
     local method="elfpy" # elfpy | elf | py
     local debug="true"
-    
-    startRdpServer
     export LD_LIBRARY_PATH="/usr/local/lib"
-    export WAYLAND_DISPLAY=$(ls $XDG_RUNTIME_DIR/wayland-?)
-    sudo chown $(id -u):$(id -g) $WAYLAND_DISPLAY
 
+    startRdpServer
     parseArgs $@
     echo fs.inotify.max_user_watches=1048575 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
     echo "set confirm off" |sudo tee ~/.gdbinit
